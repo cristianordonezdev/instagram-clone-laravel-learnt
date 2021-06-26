139166c360635f1f70d0f04fd08e0bf0cf3b1cd0
ChinoRubik <chinorubikguitar@hotmail.com>
2021-06-26 08:39:23 -0500

    Perfil hecho sin maquetacion galeria

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -5,16 +5,29 @@
     <div class="py-12 mx-auto">
         <div class=" mx-auto w-4/6 sm:px-6 cop lg:px-8 ">
 
-            <div class="bg-white overflow-hidden h-screen shadow-sm sm:rounded-lg mb-12">
+            <div class="overflow-hidden h-auto mb-12">
+                <div class="inline-block w-1/3 float-left"> 
+                    <img src="{{url('getImage/'.$user->image)}}" class=" mx-auto    rounded-full w-44 h-44 " alt="IMAGEN">
+                </div>
+
+                <div class="inline-block w-2/3"> 
+                    <p class="text-3xl">{{$user->name}} {{$user->lastname}}</p>
+                    <p class="text-xl mt-1 text-gray-500">@ {{$user->nick}} </p>
+                    <p class="mt-5 text-lg"><strong>{{count($user->images)}}</strong> <?= (count($user->images)==1) ? 'post' : 'posts'?></p>
+
+                </div>
+            </div>
+
+            <div class="border-t-2 overflow-hidden h-screen  mb-12">
                 <h1 class="text-center m-3 text-3xl block">Posts</h1>
 
 
                 <div class="grid gap-4 grid-cols-3">
                     @foreach($user->images as $img)
 
-                        <div class="">
-                            <img class="w-full h-80" src="{{url('getImagePost/'.$img->image_path)}}" alt="">
-                        </div>
+                    <div class="">
+                        <img class=" imgProfile" src="{{url('getImagePost/'.$img->image_path)}}" alt="">
+                    </div>
                     @endforeach
                 </div>
             </div>
